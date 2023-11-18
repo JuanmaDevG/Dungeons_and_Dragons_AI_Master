1373f5f283d91b334d3e86124638b62c9605b450
Juan Manuel <jmgmgonzalez57@gmail.com>
2023-11-18 17:47:42 +0100

    Added base frontend and working on style and button init js functions

---
+++ pages/game_funcs.js
@@ -0,0 +1,9 @@
+
+/*
+    Adds a player panel where to write the character's properties
+*/
+function add_player_panel()
+{
+    const players_panel = document.getElementById("players_panel");
+    //TODO
+}
